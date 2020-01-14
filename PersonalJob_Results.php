ba7b704f1b04e2bc5b4e7638d55a1b05b1aef925
Sahas Renuja Gunasekara <56167123+SahasR@users.noreply.github.com>
2020-01-14 07:55:44 +0530

    Back-end for Results Page
    There are still some minor bugs will fix it.

--- PersonalJob_Results.php
+++ PersonalJob_Results.php
@@ -15,15 +15,21 @@ $SeedNum = $_SESSION["NumSeed"];
 $NumRounds = $_SESSION["NumRounds"];
 $NumBreak = $_SESSION["NumBreak"];
 $RoundNumber = $_SESSION["RoundNumber"];
+// echo "$RoundNumber";
 $NumTeams = $_SESSION["NumTeams"];
-$Checking = "false";
+$Checking = "true";
 // echo "$NumTeams";
 // echo "$TName";
 // echo "$SeedNum";
 // echo "$NumRounds";
 // echo "$RoundNumber";
+$RoundName = "Round" . $RoundNumber;
+// echo "$RoundName";
+
+
+
 if (isset($_POST["btnResults"])) {
-	echo "Hello!";
+	// echo "Hello!";
 	$lstPropName = $_POST["lstPropName"];
 	$lstFirstPropName = $_POST["lstFirstPropName"];
 	$lstSecondPropName = $_POST["lstSecondPropName"];
@@ -32,7 +38,85 @@ if (isset($_POST["btnResults"])) {
 	$lstFirstOppName = $_POST["lstFirstOppName"];
 	$lstSecondOppName = $_POST["lstSecondOppName"];
 	$lstThirdOppName = $_POST["lstThirdOppName"];
-	echo "$lstPropName";
+
+	$txtFirstPropScore = $_POST["txtFirstPropScore"]*1;
+	$txtSecondPropScore = $_POST["txtSecondPropScore"]*1;
+	$txtThirdPropScore = $_POST["txtThirdPropScore"]*1;
+	$PropReplyScore = $_POST["PropReplyScore"]*1;
+
+
+	$txtFirstOppScore = $_POST["txtFirstOppScore"]*1;
+	$txtSecondOppScore = $_POST["txtSecondOppScore"]*1;
+	$txtThirdOppScore = $_POST["txtThirdOppScore"]*1;
+	$OppReplyScore = $_POST["OppReplyScore"]*1;
+
+	// $Checking = Validation();
+
+	if ($Checking == "true") {
+		// echo "Hello!";
+		// echo "$RoundName";
+		// echo "$txtFirstPropScore";
+		// echo "$lstFirstPropName";
+		$sql = "UPDATE speaks SET $RoundName=$txtFirstPropScore where MemberName='$lstFirstPropName'";
+  		$result = $conn->query($sql);
+
+		$sql = "UPDATE speaks SET $RoundName=$txtSecondPropScore where MemberName='$lstSecondPropName'";
+  		$result = $conn->query($sql);
+
+		$sql = "UPDATE speaks SET $RoundName=$txtThirdPropScore where MemberName='$lstThirdPropName'";
+  		$result = $conn->query($sql);
+
+		$sql = "UPDATE speaks SET $RoundName=$txtFirstOppScore where MemberName='$lstFirstOppName'";
+  		$result = $conn->query($sql);
+
+		$sql = "UPDATE speaks SET $RoundName=$txtSecondOppScore where MemberName='$lstSecondOppName'";
+  		$result = $conn->query($sql);
+
+		$sql = "UPDATE speaks SET $RoundName=$txtThirdOppScore where MemberName='$lstThirdOppName'";
+  		$result = $conn->query($sql);
+
+  		$PropTotalSpeaks = $txtFirstPropScore + $txtSecondPropScore + $txtThirdPropScore + $PropReplyScore;
+  		$OppTotalSpeaks = $txtFirstOppScore + $txtSecondOppScore + $txtThirdOppScore + $OppReplyScore;
+
+  		$sql = "SELECT TotalScore FROM wins WHERE TeamName = '$lstPropName'";
+  		$result = $conn->query($sql);
+  		while($row = $result->fetch_assoc()) {
+  			$Score = $row["TotalScore"];
+  		}
+  		$Score = $Score + $PropTotalSpeaks;
+  		$sql = "UPDATE wins SET TotalScore = $Score WHERE TeamName = '$lstPropName'";
+  		$result = $conn->query($sql);
+
+  		$sql = "SELECT TotalScore FROM wins WHERE TeamName = '$lstOppName'";
+  		$result = $conn->query($sql);
+  		while($row = $result->fetch_assoc()) {
+  			$Score = $row["TotalScore"];
+  		}
+  		$Score = $Score + $OppTotalSpeaks;
+  		$sql = "UPDATE wins SET TotalScore = $Score WHERE TeamName = '$lstOppName'";
+  		$result = $conn->query($sql);
+
+  		if ($PropTotalSpeaks > $OppTotalSpeaks) {
+  			$sql = "SELECT Wins FROM wins WHERE TeamName = '$lstPropName'";
+  			$result = $conn->query($sql);
+  			while ($row = $result->fetch_assoc()) {
+  				$UpdatedWins = $row["Wins"];
+  			}
+  			$UpdatedWins = $UpdatedWins + 1;
+  			$sql = "UPDATE wins SET Wins = $UpdatedWins WHERE TeamName = '$lstPropName'";
+  			$result = $conn->query($sql);
+  		}
+  		else {
+  			$sql = "SELECT Wins FROM wins WHERE TeamName = '$lstOppName'";
+  			$result = $conn->query($sql);
+  			while ($row = $result->fetch_assoc()) {
+  				$UpdatedWins = $row["Wins"];
+  			}
+  			$UpdatedWins = $UpdatedWins + 1;
+  			$sql = "UPDATE wins SET Wins = $UpdatedWins WHERE TeamName = '$lstOppName'";
+  			$result = $conn->query($sql);  			
+  		}
+	}
 
 }
 
@@ -45,12 +129,19 @@ function Validation() {
 <html>
 <head>
 	<title>Results Page!</title>
+<!-- 	<script type="text/javascript">
+		function LetsGo() {
+			alert("Hello");
+			var $Test = document.getElementByID('lstPropName').value;
+			alert($Test);
+		}
+	</script> -->
 </head>
 <body>
+	<form  method="post">
 	<div style="text-align: center;">
 		<div style="float: left; background-color: cyan; width: 50%">
-
-		<form method="POST">	
+	
 			<table>
 				<tr>
 					<td colspan="2">Proposition</td>				
@@ -68,7 +159,7 @@ function Validation() {
 								while ($row = $result->fetch_assoc()) {
 								$lstPropName = $row['TeamName']; 
     						?>
-    						<option value="<?php echo $lstPropName;?>"><?php echo $lstPropName;?></option>
+    						<option value="<?php echo $row['TeamName'];?>"><?php echo $row['TeamName'];?></option>
 
     						<?php
     							} 
@@ -145,11 +236,9 @@ function Validation() {
 					</td>
 					<td><input type="text" name="PropReplyScore" id="PropReplyScore"></td>
 				</tr>
-			</table>
-		</form>	
+			</table>	
 		</div>
-		<div style="float: right; background-color: salmon; width: 50%">
-			<form method="POST">	
+		<div style="float: right; background-color: salmon; width: 50%">	
 			<table>
 				<tr>
 					<td colspan="2">Opposition</td>				
@@ -248,13 +337,12 @@ function Validation() {
 					<td><input type="text" name="OppReplyScore" id="OppReplyScore"></td>
 				</tr>
 			</table>
-		</form>	
 		</div>
-		<form method="POST">
+		
 		<div>
-			<input type="submit" name="btnResults" id="btnResults" value="Submit Values">
+			<input type="submit" name="btnResults" id="btnResults" value="Submit Values" onclick="LetsGo()">
 		</div>
-		</form>
 	</div>
+</form>
 </body>
 </html>
